efcb4b7607c4ee1008b50ec78f6c1d9348526db9
Kilian Kilger <kkilger@gmail.com>
2019-02-11 09:37:34 +0100

    Add license remark

--- cmake_modules/Findcppunit.cmake
+++ cmake_modules/Findcppunit.cmake
@@ -20,7 +20,7 @@
 #
 # Copyright Iowa State University 2009-2011.
 # Distributed under the Boost Software License, Version 1.0.
-# (See accompanying file LICENSE_1_0.txt or copy at
+# (See accompanying file LICENSE.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
 set(CPPUNIT_ROOT_DIR
